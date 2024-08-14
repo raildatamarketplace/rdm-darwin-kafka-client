40cca7c8c5f2be3cb1330169bd1b6247b64c8eec
mylesadamsonrdg <myles.adamson@raildeliverygroup.com>
2024-08-14 15:34:14 +0100

    First commit of the Kafka client repository

---
+++ README.md
@@ -0,0 +1,19 @@
+# RDM Darwin Kafka Client
+
+This is a Kakfa client written in Python for consuming the Darwin Real Time Train Information PubSub data product in the Rail Data Marketplace
+
+## Description
+
+This Kafka client was created to help consumers of the Darwin Real Time Train Information PubSub data product in consuming the data
+
+## Authors
+
+Myles Adamson
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details
+
+## Support
+
+For any questions or queries please email support@raildata.org.uk
---
+++ darwin-kafka-client.py
@@ -0,0 +1,58 @@
+# Code snippet for Kafka client in Python
+
+from confluent_kafka import Consumer
+from pathlib import Path
+import argparse
+import warnings
+warnings.filterwarnings('ignore')
+import json
+
+BOOTSTRAP_SERVER = ''
+GROUP_ID = 'rdm-darwin-kafka-group'
+SASL_USERNAME = ''
+SASL_PASSWORD = ''
+SSL_CA_LOCATION = ''
+TOPIC_NAME = ''
+
+config = {
+    'bootstrap.servers': BOOTSTRAP_SERVER,
+    'group.id': GROUP_ID,
+    'security.protocol': 'SASL_SSL',
+    'sasl.mechanism': 'PLAIN',
+    'sasl.username': SASL_USERNAME,
+    'sasl.password': SASL_PASSWORD,
+    'ssl.ca.location': SSL_CA_LOCATION,
+    'auto.offset.reset': 'earliest'
+}
+
+consumer = Consumer(config)
+consumer.subscribe([TOPIC_NAME])
+
+l = []
+count = 0
+try:
+    while True:
+        msg = consumer.poll(1.0)
+        if msg is not None and msg.error() is None:
+            key = msg.key()
+            value = msg.value()
+            try:
+                if key is not None:
+                    key = key.decode('utf-8')
+            except:
+                key = "None"
+            try:
+                if value is not None:
+                    value = value.decode('utf-8')
+            except:
+                value = "None"
+            # print("key = {key:12} value = {value:12}".format(key, value))
+            print(key, value)
+            l.append(value)
+            count+=1
+            print(count)
+except KeyboardInterrupt:
+    pass
+finally:
+    consumer.close()
+
